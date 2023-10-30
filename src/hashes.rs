68817285a6f9b857f1c163c089b93e47520c4906
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-31 07:43:11 +0900

    feat: add Hashes module

---
+++ src/hashes.rs
@@ -0,0 +1,42 @@
+use std::fmt;
+
+use serde::{
+    de::{self, Visitor},
+    Deserialize, Deserializer,
+};
+
+#[derive(Debug, Clone)]
+pub struct Hashes(Vec<[u8; 20]>);
+struct HashesVisitor;
+
+impl<'de> Visitor<'de> for HashesVisitor {
+    type Value = Hashes;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a byte string whose length is a multiple of 20")
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v.len() % 20 != 0 {
+            return Err(E::custom(format!("length is {}", v.len())));
+        }
+
+        Ok(Hashes(
+            v.chunks_exact(20)
+                .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                .collect(),
+        ))
+    }
+}
+
+impl<'de> Deserialize<'de> for Hashes {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(HashesVisitor)
+    }
+}
