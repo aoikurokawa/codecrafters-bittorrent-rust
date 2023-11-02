8fbd21cbe4db3f7f9a8cb2926e0bd9d0648db486
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-02 09:00:23 +0900

    feat: implementing download pieces

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::{net::SocketAddrV4, path::PathBuf};
 
 use anyhow::Context;
 use bittorrent_starter_rust::{
-    peer::Handshake,
+    peer::{Handshake, Message, MessageFramer, MessageTag},
     torrent::{Keys, Torrent},
     tracker::{TrackerRequest, TrackerResponse},
 };
 use clap::{Parser, Subcommand};
+use futures_util::{SinkExt, StreamExt};
 use serde_bencode;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+const PIECE_MAX: usize = 1 << 14;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -19,11 +22,27 @@ struct Args {
 }
 
 #[derive(Debug, Subcommand)]
+#[clap(rename_all = "snake_case")]
 enum Command {
-    Decode { value: String },
-    Info { torrent: PathBuf },
-    Peers { torrent: PathBuf },
-    Handshake { torrent: PathBuf, peer: String },
+    Decode {
+        value: String,
+    },
+    Info {
+        torrent: PathBuf,
+    },
+    Peers {
+        torrent: PathBuf,
+    },
+    Handshake {
+        torrent: PathBuf,
+        peer: String,
+    },
+    DownloadPiece {
+        #[arg(short)]
+        output: PathBuf,
+        torrent: PathBuf,
+        piece: usize,
+    },
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -32,7 +51,7 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     match args.command {
-        Command::Decode { value } => {
+        Command::Decode { value: _ } => {
             // let v: serde_json::Value = serde_bencode::from_str(&value).unwrap();
             // println!("{}", v);
             unimplemented!("serde_bencode -> serde_json::Value is borked");
@@ -128,6 +147,102 @@ async fn main() -> anyhow::Result<()> {
             assert_eq!(&handshake.bittorent, b"BitTorrent protocol");
 
             println!("Peer ID: {}", hex::encode(&handshake.peer_id));
+        }
+        Command::DownloadPiece {
+            output,
+            torrent,
+            piece,
+        } => {
+            let dot_torrent = std::fs::read(torrent).context("open torrent file")?;
+            let t: Torrent =
+                serde_bencode::from_bytes(&dot_torrent).context("parse torrent file")?;
+
+            let length = if let Keys::SingleFile { length } = t.info.keys {
+                length
+            } else {
+                todo!();
+            };
+            assert_eq!(piece, t.info.pieces.0.len());
+
+            let info_hash = t.info_hash();
+            let request = TrackerRequest {
+                peer_id: String::from("00112233445566778899"),
+                port: 6881,
+                uploaded: 0,
+                downloaded: 0,
+                left: length,
+                compact: 1,
+            };
+
+            let url_params =
+                serde_urlencoded::to_string(&request).context("url-encode tracker parameters")?;
+            let tracker_url = format!(
+                "{}?{}&info_hash={}",
+                t.announce,
+                url_params,
+                &urlencode(&info_hash)
+            );
+
+            let response = reqwest::get(tracker_url).await.context("query tracker")?;
+            let response = response.bytes().await.context("fetch tracker response")?;
+            let tracker_info: TrackerResponse =
+                serde_bencode::from_bytes(&response).context("parse tracker response")?;
+
+            let peer = &tracker_info.peers.0[0];
+            let mut peer = tokio::net::TcpStream::connect(peer)
+                .await
+                .context("connect to peer")?;
+            let mut handshake = Handshake::new(info_hash, *b"00112233445566778899");
+            {
+                let handshake_bytes =
+                    &mut handshake as *mut Handshake as *mut [u8; std::mem::size_of::<Handshake>()];
+                let handshake_bytes: &mut [u8; std::mem::size_of::<Handshake>()] =
+                    unsafe { &mut *handshake_bytes };
+
+                peer.write_all(handshake_bytes)
+                    .await
+                    .context("write handshake")?;
+
+                peer.read_exact(handshake_bytes)
+                    .await
+                    .context("read handshake")?;
+            }
+
+            assert_eq!(handshake.length, 19);
+            assert_eq!(&handshake.bittorent, b"BitTorrent protocol");
+
+            println!("Peer ID: {}", hex::encode(&handshake.peer_id));
+
+            let mut peer = tokio_util::codec::Framed::new(peer, MessageFramer);
+            let bitfield = peer
+                .next()
+                .await
+                .expect("peer always send a bitfield")
+                .context("peer message was invalid")?;
+            assert_eq!(bitfield.tag, MessageTag::Bitfield);
+            // NOTE: we assume that the bitfield covers all pieces.
+
+            peer.send(Message {
+                tag: MessageTag::Interested,
+                payload: Vec::new(),
+            })
+            .await
+            .context("send interested message")?;
+
+            let unchoke = peer
+                .next()
+                .await
+                .expect("peer always send an unchoke")
+                .context("peer message was invalid")?;
+            assert_eq!(unchoke.tag, MessageTag::Unchoke);
+            assert!(unchoke.payload.is_empty());
+
+            let piece_hash = t.info.pieces.0[piece];
+            let piece_size = if piece == t.info.pieces.0.len() + 1 {
+                length % PIECE_MAX
+            } else {
+                PIECE_MAX
+            };
         }
     }
 
--- src/peer.rs
+++ src/peer.rs
@@ -1,3 +1,8 @@
+use bytes::{Buf, BufMut, BytesMut};
+use serde::{Deserialize, Serialize};
+use tokio::io::AsyncWriteExt;
+use tokio_util::codec::{Decoder, Encoder};
+
 #[repr(C)]
 pub struct Handshake {
     pub length: u8,
@@ -16,5 +21,137 @@ impl Handshake {
             info_hash,
             peer_id,
         }
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[repr(u8)]
+pub enum MessageTag {
+    Choke = 0,
+    Unchoke = 1,
+    Interested = 2,
+    NotInterested = 3,
+    Have = 4,
+    Bitfield = 5,
+    Request = 6,
+    Piece = 7,
+    Cancel = 8,
+}
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    pub tag: MessageTag,
+    pub payload: Vec<u8>,
+}
+
+pub struct MessageFramer;
+
+const MAX: usize = 1 << 16;
+
+impl Decoder for MessageFramer {
+    type Item = Message;
+    type Error = std::io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        if src.len() < 4 {
+            // Not enough data to read length marker + tag.
+            return Ok(None);
+        }
+
+        // Read length marker.
+        let mut length_bytes = [0u8; 4];
+        length_bytes.copy_from_slice(&src[..4]);
+        let length = u32::from_be_bytes(length_bytes) as usize;
+
+        if length == 0 {
+            // this is a heartbeat message.
+            // discard it.
+            src.advance(4);
+            // and then try again in case the buffer has more messages
+            return self.decode(src);
+        }
+
+        if src.len() < 5 {
+            // Not enough data to read tag marger.
+            return Ok(None);
+        }
+
+        // Check that the length is not too large to avoid a denial of
+        // service attack where the server runs out of memory.
+        if length > MAX {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", length),
+            ));
+        }
+
+        if src.len() < 4 + length {
+            // The full string has not yet arrived.
+            //
+            // We reserve more space in the buffer. This is not strictly
+            // necessary, but is a good idea performance-wise.
+            src.reserve(4 + length - src.len());
+
+            // We inform the Framed that we need more bytes to form the next
+            // frame.
+            return Ok(None);
+        }
+
+        // Use advance to modify src such that it no longer contains
+        // this frame.
+        let tag = match src[4] {
+            0 => MessageTag::Choke,
+            1 => MessageTag::Unchoke,
+            2 => MessageTag::Interested,
+            3 => MessageTag::NotInterested,
+            4 => MessageTag::Have,
+            5 => MessageTag::Bitfield,
+            6 => MessageTag::Request,
+            7 => MessageTag::Piece,
+            8 => MessageTag::Cancel,
+            tag => {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("unknown message type {}", tag),
+                ))
+            }
+        };
+        let data = src[5..4 + length - 1].to_vec();
+        src.advance(4 + length);
+
+        Ok(Some(Message {
+            tag: tag as MessageTag,
+            payload: data,
+        }))
+    }
+}
+
+// struct MessageFramer {}
+
+impl Encoder<Message> for MessageFramer {
+    type Error = std::io::Error;
+
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        // Don't send a message if it is longer than the other end will
+        // accept.
+        if item.payload.len() + 1 > MAX {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", item.payload.len()),
+            ));
+        }
+
+        // Convert the length into a byte array.
+        // The cast to u32 cannot overflow due to the length check above.
+        let len_slice = u32::to_be_bytes(item.payload.len() as u32 + 1);
+
+        // Reserve space in the buffer.
+        dst.reserve(4 + 1 + item.payload.len());
+
+        // Write the length and string to the buffer.
+        dst.extend_from_slice(&len_slice);
+        dst.put_u8(item.tag as u8);
+        dst.extend_from_slice(&item.payload);
+        Ok(())
     }
 }
