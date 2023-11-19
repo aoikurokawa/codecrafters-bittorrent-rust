5eab84c660e0a50d0fa568a39f844b12548a6e92
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-20 07:08:37 +0900

    feat: bencode decode number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env;
 
 fn decode_bencode_value(encoded_value: &str) -> serde_json::Value {
-    if let Some((len, rest)) = encoded_value.split_once(':') {
+    if let Some(rest) = encoded_value.strip_prefix('i') {
+        if let Some((digits, _)) = rest.split_once('e') {
+            if let Ok(n) = digits.parse::<i64>() {
+                return n.into();
+            }
+        }
+    } else if let Some((len, rest)) = encoded_value.split_once(':') {
         if let Ok(len) = len.parse::<usize>() {
             return serde_json::Value::String(rest[..len].to_string());
         }
     }
+
     panic!("Unhandled encoded value: {}", encoded_value);
 }
 
