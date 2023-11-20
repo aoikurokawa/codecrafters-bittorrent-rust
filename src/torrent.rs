cb5dd8940dc2af7967029228cef13e4b342ef679
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-21 08:46:12 +0900

    chore: split files

---
+++ src/torrent.rs
@@ -0,0 +1,172 @@
+use std::fmt;
+
+use serde::de::{self, Visitor};
+use serde::ser::Serializer;
+use serde::{Deserialize, Deserializer, Serialize};
+
+pub fn decode_bencode_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    match encoded_value.chars().next() {
+        Some('i') => {
+            if let Some((n, rest)) =
+                encoded_value
+                    .split_at(1)
+                    .1
+                    .split_once('e')
+                    .and_then(|(digits, rest)| {
+                        let n = digits.parse::<i64>().ok()?;
+                        Some((n, rest))
+                    })
+            {
+                return (n.into(), rest);
+            }
+        }
+        Some('l') => {
+            let mut values = Vec::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (v, remainder) = decode_bencode_value(rest);
+                values.push(v);
+                rest = remainder;
+            }
+            return (values.into(), &rest[1..]);
+        }
+        Some('d') => {
+            let mut dict = serde_json::Map::new();
+            let mut values = Vec::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (k, remainder) = decode_bencode_value(rest);
+                let k = match k {
+                    serde_json::Value::String(k) => k,
+                    k => {
+                        panic!("key must be strings, not {k:?}");
+                    }
+                };
+                let (v, remainder) = decode_bencode_value(remainder);
+                dict.insert(k, v.clone());
+                values.push(v);
+                rest = remainder;
+            }
+            return (dict.into(), &rest[1..]);
+        }
+        Some('0'..='9') => {
+            if let Some((len, rest)) = encoded_value.split_once(':').and_then(|(len, rest)| {
+                let len = len.parse::<usize>().ok()?;
+                Some((len, rest))
+            }) {
+                return (rest[..len].to_string().into(), &rest[len..]);
+            }
+        }
+        _ => {}
+    }
+    panic!("Unhandled encoded value: {}", encoded_value);
+}
+
+/// A Metainfo files (also known as .torrent files).
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Torrent {
+    /// The URL of the tracker
+    pub announce: String,
+
+    pub info: Info,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct Info {
+    /// The suggested name to save the file (or directory) as. It is purely advisory.
+    ///
+    /// In the single file case, the name key is the name of a file, in the multiple file case,
+    /// it's the nmae of a directory.
+    pub name: String,
+
+    /// The number of bytes in each piece the file is split into.
+    ///
+    /// For the purposes of transfer, files are split into fixed-size pieces which are all the same
+    /// length except for possibly the last one which may be truncated. piece length is almost
+    /// always a power of two, most commonly 2^18 = 256K
+    /// (BitTorrent prior to version 3.2 uses 2^20 = 1 M as default).
+    #[serde(rename = "piece length")]
+    pub plength: usize,
+
+    /// Each of which is the SHA1 hash of the piece at the corresponding index.
+    pub pieces: Hashes,
+
+    #[serde(flatten)]
+    pub keys: Keys,
+}
+
+/// There is also a key `length` or a key `files`, but not both or neither.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum Keys {
+    /// If `length` is present then the download represents a single file,
+    SingleFile {
+        /// The length of the file in bytes.
+        length: usize,
+    },
+
+    /// Otherwise it represents a set of files which go in a directory structure.
+    /// For the purposes of the other keys in `Info`, the multi-file case is treated as only having
+    /// a single file by concatenating the files in the order they appear in the files list.
+    MultiFile {
+        /// The files list is the value files maps to, and is a list of dictionaries containing the
+        /// following keys:
+        files: Vec<File>,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct File {
+    /// The length of the file, in bytes
+    pub length: usize,
+
+    /// Subdirectory names, the last of which is the actual file name
+    /// (a zero length list is an error case).
+    pub path: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Hashes(pub Vec<[u8; 20]>);
+struct HashesVisitor;
+
+impl<'de> Visitor<'de> for HashesVisitor {
+    type Value = Hashes;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a byte string whose length is multiple of 20")
+    }
+
+    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        if v.len() % 20 != 0 {
+            return Err(E::custom(format!("length is {}", v.len())));
+        }
+
+        Ok(Hashes(
+            v.chunks_exact(20)
+                .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                .collect(),
+        ))
+    }
+}
+
+impl<'de> Deserialize<'de> for Hashes {
+    fn deserialize<D>(deserializer: D) -> Result<Hashes, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(HashesVisitor)
+    }
+}
+
+impl Serialize for Hashes {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let single_file = self.0.concat();
+        serializer.serialize_bytes(&single_file)
+    }
+}
